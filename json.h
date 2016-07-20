e9d8ae5991806fc3199930d2016e93e128b845c3
mb2dev <bordage.mickael@gmail.com>
2016-07-20 11:55:58 +0200

    WP-166 #Correction nom des méthodes

--- json.h
+++ json.h
@@ -3,8 +3,14 @@
 
 #include "hashmap.h"
 
+typedef struct indexValue {
+    int a;
+    char* b;
+}t_indexValue;
+
 
 t_hashmap* JSON_parse(char* string) ; // Convert a JSON String to a HashMap representation.
 char* JSON_stringify(t_hashmap* map) ; // Convert a HashMap to a JSON String representation.
+t_indexValue* JSON_getKey(int index, char* json);
 
 #endif
--- main.c
+++ main.c
@@ -5,5 +5,7 @@
 #include "nosql.h"
 
 int main(){
+  char json[] = "{ name : 'Gabriel',  rate : 12,  group : 'AL2' }";
+  JSON_parse(json);
   return 0;
 }
