19f8ba60d8bb15d2374883b25732ca544e33ddf5
Ptitpaindepice <gabrielvautrin@gmail.com>
2016-07-27 20:57:05 +0200

    added json.c

--- json.h
+++ json.h
@@ -3,9 +3,29 @@
 
 #include "hashmap.h"
 
+typedef enum Type Type;
+enum Type
+{
+    INT,BOOLEAN, STRING,FLOAT,DATE
+};
 
-t_hashmap* JSON_parse(char* string) ; // Convert a JSON String to a HashMap representation.
-t_hashmap* JSON_parse_sub(char* root, char* string, t_hashmap* map);
+typedef struct indexValue {
+    int a;
+    char* b;
+
+}t_indexValue;
+
+
+t_hashmap* JSON_parse(char* string, t_hashmap* map) ; // Convert a JSON String to a HashMap representation.
 char* JSON_stringify(t_hashmap* map) ; // Convert a HashMap to a JSON String representation.
+t_indexValue* JSON_getKey(int index, char* json);
+int getKeyValue(char* json);
+void getKey(char* json,t_hashmap* map);
+int jsonError(char * json);
+char* removeSpace(char source[]);
+char* removeQuote(char source[]);
+char* printType(Type type);
+int check_string( char* string);
+void myStrcopy(char *copy, char* target, int size);
 
 #endif
